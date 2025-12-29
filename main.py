f64fd3a8838b4cfb68469461ef2a5454e295f3d4
Wasi <ragib3266@gmail.com>
2025-12-29 18:17:13 -0500

    added flask so render doesnt throw error

--- main.py
+++ main.py
@@ -6,7 +6,24 @@ import requests
 import wbgapi as wb
 import random
 import aiohttp
+from flask import Flask
+from threading import Thread 
 
+app = Flask('')
+@app.route('/')
+def home():
+    return "Works"
+
+def run():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.daemon = True
+    t.start()
+
+keep_alive()
 
 
 
